e6ec119cd78bcff4830a852581d8a67eba206456
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-04-10 00:20:37 +0900

    rails3.2環境作成

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.2'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsFlextures::Application.config.secret_token = '08eabaa4f79a229ec25d99226e781fe402f0b19f0ff1874bb38229ec481b9c706186ac46f82f3cb98e1838f795c539eac56f598478f59ed859001075b9cd7162'
