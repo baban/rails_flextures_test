9c37047a26640ca49702c0be925ee2c76203a8ca
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-04-11 23:11:51 +0900

    dumpモジュールのバグ修正

--- spec/models/flextures_dumper.rb
+++ spec/models/flextures_dumper.rb
@@ -6,19 +6,18 @@ require "fileutils"
 describe Flextures do
   describe "Dumper::" do
     before do
-      `rm spec/fixtures/users.csv`
-      `rm spec/fixtures/users.yml`
+      `rm spec/fixtures/users.csv 2>/dev/null`
+      `rm spec/fixtures/users.yml 2>/dev/null`
     end
 
     #  dumpしたデータを比較する
     describe "csv " do
-=begin
       before do
         `cp spec/fixtures_bkup/users_dump.csv spec/fixtures/users.csv`
       end
 
       describe do
-        fixtures :users
+        flextures :users
 
         it "はデータをdumpする" do
           @base = IO.read Rails.root.to_path<< "/spec/fixtures/users.csv"
@@ -27,11 +26,6 @@ describe Flextures do
           @base.should == @result
         end
       end
-
-      after do
-        `rm spec/fixtures/users.csv`
-      end
-=end
     end
 
     # dumpしたyamlを比較する
@@ -40,31 +34,91 @@ describe Flextures do
         before do
           FileUtils.cp "spec/fixtures_bkup/users_dump.yml", "spec/fixtures/users.yml"
         end
-=begin
         describe do
-          fixtures :users
+          flextures :users
           it "はyamlでデータを吐き出す" do
             @base = IO.read Rails.root.to_path<< "/spec/fixtures_bkup/users_dump.yml"
-            p User.all
             Flextures::Dumper::yml table: "users"
             @result = IO.read Rails.root.to_path<< "/spec/fixtures/users.yml"
             @result.should == @base
           end
         end
 
-        it "改行混じり" do
+        describe "改行混じり" do
+          before do
+            FileUtils.cp "spec/fixtures_bkup/users_dump_br.yml", "spec/fixtures/users.yml"
+          end
+
+          context do
+            flextures :users
+
+            it "はyamlでデータを吐き出す" do
+              @base = IO.read Rails.root.to_path<< "/spec/fixtures_bkup/users_dump_br.yml"
+              Flextures::Dumper::yml table: "users"
+              @result = IO.read Rails.root.to_path<< "/spec/fixtures/users.yml"
+              @result.should == @base
+            end
+          end
         end
 
-        it "先頭space" do
+        context "null" do
         end
 
-        it "tab混じり" do
+        context "先頭space" do
+          before do
+            `rm spec/fixtures/users.csv 2>/dev/null`
+            `rm spec/fixtures/users.yml 2>/dev/null`
+            FileUtils.cp "spec/fixtures_bkup/users_dump_space.csv", "spec/fixtures/users.csv"
+          end
+          flextures :users
+
+          it "YAMLは読み込み可能" do
+            Flextures::Dumper::yml table: "users"
+            expect { YAML.load_file( Rails.root.to_path+"/spec/fixtures/users.yml" ) }.not_to raise_error
+          end
+
+          it "先頭のスペースを削除した状態にする" do
+            Flextures::Dumper::yml table: "users"
+            yaml = YAML.load_file( Rails.root.to_path+"/spec/fixtures/users.yml" )
+            yaml["users_0"]["name"].should == "bcde"
+          end
+
+          it "先頭がtabでも削除" do
+            Flextures::Dumper::yml table: "users"
+            yaml = YAML.load_file( Rails.root.to_path+"/spec/fixtures/users.yml" )
+            yaml["users_3"]["name"].should == "llll"
+            yaml["users_4"]["name"].should == "mm\nmm"
+          end
         end
 
-        it "" do
+        context "tab混じり" do
+          context "tabを２スペースに変換" do
+            before do
+              `rm spec/fixtures/users.csv 2>/dev/null`
+              `rm spec/fixtures/users.yml 2>/dev/null`
+              FileUtils.cp "spec/fixtures_bkup/users_dump_tab.csv", "spec/fixtures/users.csv"
+            end
+            flextures :users
+
+            it "YAMLは読み込み可能" do
+              Flextures::Dumper::yml table: "users"
+              expect { YAML.load_file( Rails.root.to_path+"/spec/fixtures/users.yml" ) }.not_to raise_error
+            end
+
+            it "tabを２スペースに変換" do
+              Flextures::Dumper::yml table: "users"
+              yaml = YAML.load_file( Rails.root.to_path+"/spec/fixtures/users.yml" )
+              yaml["users_0"]["name"].should == "ii a  i"
+            end
+
+          end
         end
-=end
       end
+    end
+    after do
+      `rm spec/fixtures/users.csv 2>/dev/null`
+      `rm spec/fixtures/users.yml 2>/dev/null`
+      FileUtils.cp "spec/fixtures_bkup/users_dump.yml", "spec/fixtures/users.yml"
     end
   end
 end
