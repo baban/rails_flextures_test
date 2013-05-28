3331724b0ede1faf8559ac9ab35d90b11beb4373
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-05-28 23:52:41 +0900

    flextures

--- db/migrate/20130403141452_create_refinery_roles_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateRefineryRolesUsers < ActiveRecord::Migration
-  def change
-    create_table :refinery_roles_users, :id => false  do |t|
-      t.integer :user_id
-      t.integer :role_id
-      t.timestamps
-    end
-  end
-end
--- spec/models/flextures_command_spec.rb
+++ spec/models/flextures_command_spec.rb
@@ -4,99 +4,94 @@ require 'spec_helper'
 require "fileutils"
 
 describe Flextures::Rake::Command do
-  context "データダンプ関連" do
+  describe "rake db::flextures::dump" do
     before do
       `rm spec/fixtures/* 2>/dev/null`
       `cp spec/fixtures_bkup2/* spec/fixtures/ 2>/dev/null`
     end
 
-    describe "::dump" do
-      context "FORMAT指定なし" do
-        context "テーブル名で絞ってある" do
-          it " TABLE=テーブル名 を設定している場合 " do
-            ENV["TABLE"] = "users"
-            filenames = Flextures::Rake::Command::dump
-            filenames.should == ["spec/fixtures/users.csv"]
-            ENV.delete "TABLE"
-          end
-        end
-        context "全件吐き出し" do
-          it " TABLE=テーブル名 を設定している場合 " do
-            filenames = Flextures::Rake::Command::dump
-            filenames.should ==  ["spec/fixtures/admin_users.csv", "spec/fixtures/guilds.csv", "spec/fixtures/items.csv",
-                                  "spec/fixtures/s_user.csv", "spec/fixtures/upload_images.csv", "spec/fixtures/users.csv"]
-          end
+    context "not exist FORMAT option" do
+      context "if set TABLE option" do
+        it "file name is returned by table_name" do
+          ENV["TABLE"] = "users"
+          filenames = Flextures::Rake::Command::dump
+          filenames.should == ["spec/fixtures/users.csv"]
+          ENV.delete "TABLE"
         end
       end
-
-      context "FORMAT=csv" do
-        context "テーブル名で絞ってある" do
-          it " TABLE=テーブル名 を設定している場合 " do
-            ENV["TABLE"] = "users"
-            ENV["FORMAT"] = "csv"
-            filenames = Flextures::Rake::Command::dump
-            filenames.should == ["spec/fixtures/users.csv"]
-            ENV.delete "TABLE"
-            ENV.delete "FORMAT"
-          end
-        end
-        context "全件吐き出し" do
-          it " TABLE=テーブル名 を設定している場合 " do
-            ENV["FORMAT"]="csv"
-            filenames = Flextures::Rake::Command::dump
-            filenames.should ==  ["spec/fixtures/admin_users.csv", "spec/fixtures/guilds.csv", "spec/fixtures/items.csv",
-                                  "spec/fixtures/s_user.csv", "spec/fixtures/upload_images.csv", "spec/fixtures/users.csv"]
-            ENV.delete "FORMAT"
-          end
-        end
-      end
-
-      context "FORMAT=yml" do
-        context "テーブル名で絞ってある" do
-          it " TABLE=テーブル名 を設定している場合 " do
-            ENV["TABLE"] = "users"
-            ENV["FORMAT"] = "yml"
-            filenames = Flextures::Rake::Command::dump
-            filenames.should == ["spec/fixtures/users.yml"]
-            ENV.delete "TABLE"
-            ENV.delete "FORMAT"
-          end
-        end
-        context "全件吐き出し" do
-          it " TABLE=テーブル名 を設定している場合 " do
-            ENV["FORMAT"] = "yml"
-            filenames = Flextures::Rake::Command::dump
-            filenames.should == ["spec/fixtures/admin_users.yml", "spec/fixtures/guilds.yml", "spec/fixtures/items.yml", 
-                                 "spec/fixtures/s_user.yml", "spec/fixtures/upload_images.yml", "spec/fixtures/users.yml"]
-            ENV.delete "FORMAT"
-          end
+      context "not setted TABLE option" do
+        it " all file is retuened " do
+          filenames = Flextures::Rake::Command::dump
+          filenames.should ==  ["spec/fixtures/admin_users.csv", "spec/fixtures/guilds.csv", "spec/fixtures/items.csv",
+                                "spec/fixtures/s_user.csv", "spec/fixtures/upload_images.csv", "spec/fixtures/users.csv"]
         end
       end
     end
 
+    context "FORMAT=csv" do
+      context "if set TABLE option" do
+        it " file name is retuned by table_name " do
+          ENV["TABLE"] = "users"
+          ENV["FORMAT"] = "csv"
+          filenames = Flextures::Rake::Command::dump
+          filenames.should == ["spec/fixtures/users.csv"]
+          ENV.delete "TABLE"
+          ENV.delete "FORMAT"
+        end
+      end
+      context "not setted TABLE option" do
+        it " all file is retuened " do
+          ENV["FORMAT"]="csv"
+          filenames = Flextures::Rake::Command::dump
+          filenames.should ==  ["spec/fixtures/admin_users.csv", "spec/fixtures/guilds.csv", "spec/fixtures/items.csv",
+                                "spec/fixtures/s_user.csv", "spec/fixtures/upload_images.csv", "spec/fixtures/users.csv"]
+          ENV.delete "FORMAT"
+        end
+      end
+    end
+
+    context "FORMAT=yml" do
+      context "if set TABLE option" do
+        it " file name is retuned by table_name " do
+          ENV["TABLE"] = "users"
+          ENV["FORMAT"] = "yml"
+          filenames = Flextures::Rake::Command::dump
+          filenames.should == ["spec/fixtures/users.yml"]
+          ENV.delete "TABLE"
+          ENV.delete "FORMAT"
+        end
+      end
+      context "not setted TABLE option" do
+        it " all file is retuened " do
+          ENV["FORMAT"] = "yml"
+          filenames = Flextures::Rake::Command::dump
+          filenames.should == ["spec/fixtures/admin_users.yml", "spec/fixtures/guilds.yml", "spec/fixtures/items.yml", 
+                               "spec/fixtures/s_user.yml", "spec/fixtures/upload_images.yml", "spec/fixtures/users.yml"]
+          ENV.delete "FORMAT"
+        end
+      end
+    end
     after do
       `rm spec/fixtures/* 2>/dev/null`
       `cp spec/fixtures_bkup2/* spec/fixtures/ 2>/dev/null`
     end
   end
 
-  context "データロード関連" do
-    describe "::load" do
-      context "FORMAT指定なし" do
-        context "テーブル名で絞ってLoad" do
-          it " TABLE=テーブル名 を設定している場合 " do
-            ENV["TABLE"] = "users"
-            filenames = Flextures::Rake::Command::load
-            filenames.should == ["spec/fixtures/users.yml"]
-            ENV.delete "TABLE"
-          end
+  describe "rake db::flextures::dump" do
+    context "FORMAT option is not exist" do
+      context "if TABLE option is setted" do
+        it " file name list will return only one file " do
+          ENV["TABLE"] = "users"
+          filenames = Flextures::Rake::Command::load
+          filenames.should == ["spec/fixtures/users.yml"]
+          ENV.delete "TABLE"
         end
-        context "全件Load" do
-          it "" do
-            filenames = Flextures::Rake::Command::load
-            filenames.should == [ "spec/fixtures/admin_users.csv", "spec/fixtures/guilds.csv", "spec/fixtures/items.yml",
-                                  "spec/fixtures/s_user.csv", "spec/fixtures/upload_images.csv", "spec/fixtures/users.yml" ]
-          end
+      end
+      context "if TABLE option is not setted" do
+        it " file name list will return all table names " do
+          filenames = Flextures::Rake::Command::load
+          filenames.should == [ "spec/fixtures/admin_users.csv", "spec/fixtures/guilds.csv", "spec/fixtures/items.yml",
+                                "spec/fixtures/s_user.csv", "spec/fixtures/upload_images.csv", "spec/fixtures/users.yml" ]
         end
       end
     end
@@ -140,20 +135,6 @@ describe Flextures::Rake::Command do
           filenames.should == ["spec/fixtures/users.yml","spec/fixtures/items.yml"]
           ENV.delete "TABLE"
           ENV.delete "FORMAT"
-        end
-      end
-
-      context "全件Load" do
-        it "読み込まないテーブルはファイル名を持たないでnil" do
-          filenames = Flextures::Rake::Command::load
-          filenames.should == [
-            nil,
-            "spec/fixtures/guilds.yml",
-            "spec/fixtures/items.yml",
-            nil,
-            nil,
-            "spec/fixtures/users.yml"
-          ]
         end
       end
     end
