abfcdca45c142e3bbdee9edfd9f953484c915f68
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-06-09 15:05:01 +0900

    add test shoulda support functions

--- test/unit/load_test.rb
+++ test/unit/load_test.rb
@@ -4,12 +4,40 @@ require 'test_helper'
 
 class LoadTest < Test::Unit::TestCase
   context "context" do
-    flextures :users
-    setup do
-      p :setup
+    context "flextures function" do
+      flextures :users
+      should "test data is exist" do
+        assert_equal true, User.first.is_a?(User)
+      end
     end
-    should "テストの作動確認" do
-      p User.first
+
+    context "flextures_delete" do
+      flextures :users
+      flextures_delete :users
+      should "test data is deleted" do
+        assert_equal nil, User.first
+      end
+    end
+
+    context "::flextures_options" do
+      flextures_set_options( cache: true )
+
+      should "option is setted" do
+        Flextures::Loader::flextures_options.should == { cache: true }
+      end
+
+      context "add option" do
+        flextures_set_options( unfilter: true )
+        should "option is added" do
+          Flextures::Loader::flextures_options.should == { cache: true, unfilter: true }
+        end
+      end
+
+      context "new context" do
+        should "added option is deleted" do
+          Flextures::Loader::flextures_options.should == { cache: true }
+        end
+      end
     end
   end
 end
