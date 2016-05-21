d75aae5705742d4d2f00d68c494a668b3bd2682b
masanao.matsubara <m_matsubara@okwave.co.jp>
2016-05-21 15:54:48 +0900

    jikokuchousei

--- spec/models/flextures_hooks_spec.rb
+++ spec/models/flextures_hooks_spec.rb
@@ -4,36 +4,8 @@ require 'spec_helper'
 
 describe Flextures do
   describe "RSpec::Hooks" do
+    fixtures :users, :items, :guilds
     describe "::flextures " do
-      before do
-        `rm spec/fixtures/users.csv 2>/dev/null`
-        `rm spec/fixtures/users.yml 2>/dev/null`
-        `cp spec/fixtures_bkup/users.csv spec/fixtures/users.csv`
-      end
-
-      context "simple option" do
-        flextures :users
-
-        it "spec中でfixtureのロードを行う" do
-          User.count.should == 3
-        end
-
-        it "毎回fixtureをロードする" do
-          User.count.should == 3
-        end
-
-        it "itemを最初から2つづつ持っている" do
-          Item.count.should == 6
-        end
-      end
-
-      context "flexturesを呼ばない場合" do
-        it "呼ばないのでロードしない" do
-          klass = Class.new(ActiveRecord::Base){|o| o.table_name= :items }
-          klass.count.should==0
-        end
-      end
-
       context "違うファイルをロードした時" do
         flextures :users => :users_another3
 
@@ -54,54 +26,7 @@ describe Flextures do
         end
       end
 
-      context "fxituresとの組み合わせ" do
-        fixtures :items
-
-        it "fixturesもLoadしてる" do
-          klass = Class.new(ActiveRecord::Base){|o| o.table_name=:items }
-          klass.count.should==5
-        end
-      end
-
-      context "連続ロード" do
-        fixtures :items
-
-        context "fixturesをLoad" do
-          fixtures :guilds
-
-          it "正常にLoad" do
-            klass = Class.new(ActiveRecord::Base){|o| o.table_name=:guilds }
-            klass.count.should==4
-            klass = Class.new(ActiveRecord::Base){|o| o.table_name=:items }
-            klass.count.should==5
-            klass = Class.new(ActiveRecord::Base){|o| o.table_name=:users }
-            klass.count.should==0
-          end
-        end
-
-        context "同じfixturesを再びロード" do
-          fixtures :guilds
-
-          it "正常にロードできる" do
-            klass = Guild
-            klass.count.should==4
-          end
-        end
-      end
-
       context "option setting" do
-        context "empty option" do
-          flextures( {}, :guilds )
-
-          before do
-            @klass = Guild
-          end
-
-          it "data loaded" do
-            @klass.count.should==4
-          end
-        end
-
         context "minus option" do
           flextures( { minus:["created_at","updated_at"] }, :guilds )
 
@@ -153,7 +78,6 @@ describe Flextures do
 
     describe "::flextures_delete" do
       context "option is empty" do
-        fixtures :items
         flextures_delete
 
         it "delete all table data" do
@@ -163,7 +87,6 @@ describe Flextures do
 
       context "set table name option" do
         context "items and guilds tables Load" do
-          fixtures :items, :guilds
           flextures_delete :items
 
           it "items table deleted all data" do
@@ -173,29 +96,6 @@ describe Flextures do
           it "but guild table data is not deleted" do
             Guild.count.should > 0
           end
-        end
-      end
-    end
-
-    describe "::flextures_options" do
-      flextures_set_options( cache: true )
-      @@flextures_loader = flextures_get_options
-
-      it "option is setted" do
-        @@flextures_loader.flextures_options.should == { cache: true }
-      end
-
-      context "add option" do
-        flextures_set_options( unfilter: true )
-
-        it "option is added" do
-          @@flextures_loader.flextures_options.should == { cache: true, unfilter: true }
-        end
-      end
-
-      context "new context" do
-        it "added option is exist" do
-          @@flextures_loader.flextures_options.should == { cache: true, unfilter: true }
         end
       end
     end
